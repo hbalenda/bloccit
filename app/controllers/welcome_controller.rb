c4fc682b50f9571b8b70422725b885eb5c06d07c
hannah c balenda <hbalenda@Hannahs-Macbook-Pro.local>
2016-01-26 17:33:49 -0800

    Add contact view and update welcome_controller.rb and routes.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,4 +5,7 @@ class WelcomeController < ApplicationController
 
   def about
   end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'welcome/about'
+
+  get 'welcome/contact'
 #declares default page
   root 'welcome#index'
 end

bb277ed43cf4438ebabc4b90a44f51db566b93ab
hannah c balenda <hbalenda@Hannahs-Macbook-Pro.local>
2016-02-02 15:21:57 -0800

    Add random data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do
+  Post.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+100.times do
+  Comment.create!(
+
+  post: posts.sample,
+  body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts"#{Post.count} posts created"
+puts "#{Comment.count} comments created"

19a569904516175a136938569ded353befa296ed
hcbviolet <hcbviolet@gmail.com>
2016-02-03 14:17:26 -0800

    Add create and show functions to posts_controller and posts_controlller_spec.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,28 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    #create a new instance of post
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    #if post is successfully saved, display success message
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      #if save is unsuccessful, display error message and render the new view again
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit

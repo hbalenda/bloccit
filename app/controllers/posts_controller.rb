07ecafff6f34a3ba06445b24235aea6ee73ece58
hannah c balenda <hbalenda@Hannahs-Macbook-Pro.local>
2016-02-03 11:41:27 -0800

    Add spam filter method to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = spam(Post.all)
   end
 
   def show
@@ -11,4 +11,14 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  def spam(posts)
+    posts.each_with_index.map do |post, i|
+      if i == 0 || i % 5 == 0
+        post[:title] = "SPAM"
+      end
+    end
+    posts
+  end
+
 end

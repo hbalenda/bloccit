12221675ab057f2d8f5df6c188f6df9366eea245
hcbviolet <hcbviolet@gmail.com>
2016-02-09 13:28:23 -0800

    Validate topics. Create form partial for topics.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,5 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
 
   it { is_expected.to have_many(:posts) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_length_of(:name).is_at_least(5) }
+  it { is_expected.to validate_length_of(:description).is_at_least(15) }
 
   describe "attributes" do
     it "responds to name" do

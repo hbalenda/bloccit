7f48842f64197a7a7e47bec6abc8dd6defac0d90
hcbviolet <hcbviolet@gmail.com>
2016-02-10 13:08:38 -0800

    Add static confirm user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,5 +20,11 @@ class UsersController < ApplicationController
   end
 
   def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ end
 
   resources :users, only: [:new, :create]
 
+  post 'users/confirm' => 'users#confirm'
+
   get 'about' => 'welcome#about'
 #declares default page
   root 'welcome#index'

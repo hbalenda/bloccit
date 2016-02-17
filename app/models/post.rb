7a98975380c1bcd95b9b7c3b9ba8c683631ee0c0
hcbviolet <hcbviolet@gmail.com>
2016-02-16 19:05:32 -0800

    votes (in progress)

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
+  has_many :votes, dependent: :destroy
 
   default_scope { order('created_at DESC') }
 
@@ -11,4 +12,16 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,13 +27,16 @@ topics = Topic.all
 
 
 50.times do
-  Post.create!(
+  post = Post.create!(
     user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
 end
+
+post.update_attribute(:created_at, rand:(10.minutes .. 1.year).ago)
+rand(1..5).times {post.votes.create!(value:[-1,1].sample, user: users.sample) }
 posts = Post.all
 
 100.times do
@@ -62,3 +65,4 @@ puts "#{Post.count} posts created"
 puts "#{Topic.count} topics created"
 puts "#{Comment.count} comments created"
 puts "#{User.count} users created"
+puts "#{Vote.count} votes created"

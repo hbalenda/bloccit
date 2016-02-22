f67cd6a278b4854ec81f0700c87e15600bd7c1b6
hcbviolet <hcbviolet@gmail.com>
2016-02-22 10:37:05 -0800

    Build an API (part 2)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,4 +11,22 @@ class Api::V1::UsersController < Api::V1::BaseController
     users = User.all
     render json: users.to_json, status: 200
   end
+
+  def create
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: user.to_json, status: 200
+    else
+      render json: {error: "User update failed", status: 400}, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :role)
+  end
 end

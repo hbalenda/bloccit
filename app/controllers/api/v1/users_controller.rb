026bc52bae22ef316a28c1b213a3aef4ab35d5b9
hcbviolet <hcbviolet@gmail.com>
2016-02-22 12:42:32 -0800

    Build an API (part 2)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,14 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def create
+    user = User.new(user_params)
+
+    if user.valid?
+      user.save!
+      render json: user.to_json, status: 201
+    else
+      render json: {error: "User is invalid", status: 400}, status: 400
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, only: [:index, :show]
+      resources :topics, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
